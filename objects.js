7891ca1a230eae4eb341c1c79c2d3a3284f61243
JatinRajvani <jatinrajvani.cg@gmail.com>
2024-08-22 14:16:33 +0530

    objects.js

---
+++ objects.js
@@ -0,0 +1,12 @@
+// The objects store the property of any thing together
+const car={
+Name:"Swift", Model:2018, Color:"black", weight:"500kg"
+}
+console.log(car)
+// Here if we have to find only any one thing about car we find out it by this way 
+console.log(car["Model"])
+
+// for adding a new property in object
+object["District"]="B.k" 
+console.log(object)
+
